2601abe3be21415f8c7830c50c6d40e970b1421c
Gracia Lukelo <gracialukelo@outlook.com>
2024-01-24 09:55:41 +0100

    first commit

---
+++ app/src/main/java/com/example/httprequest/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.httprequest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        data = findViewById(R.id.text_request);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://reqres.in/api/users?page=2";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            data.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+}
